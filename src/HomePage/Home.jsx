3a93d48dc3a4eabdf78e8f40c895bf17d43a802e
Abhinesh <abhineshgoud72@gmail.com>
2024-05-19 18:41:17 +0530

    create medical specialist, caring, latestNews blocks

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,6 +1,10 @@
 import { Header } from "../components/Header";
+import FindSpecialisation from "./FindSpecialisation";
 import Hero from "./Hero";
 import OfferCards from "./OfferCards";
+import MedicalSpecialist from "./MedicalSpecialist";
+import PatientCaring from "./PatientCaring";
+import LatestNews from "./LatestNews";
 
 function Home() {
   const home = true;
@@ -9,6 +13,10 @@ function Home() {
       <Header home={home} />
       <Hero> </Hero>
       <OfferCards />
+      <FindSpecialisation />
+      <MedicalSpecialist />
+      <PatientCaring />
+      <LatestNews />
     </div>
   );
 }
