7da6ff2be03612734484d1d796208d8475f11d8f
Abhinesh <abhineshgoud72@gmail.com>
2024-05-23 20:26:41 +0530

    added browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import DetailPage from "./DetailPage/DetailPage.jsx";
 import Home from "./HomePage/Home";
 import MyBookingPage from "./MyBookingPage/MyBookingPage.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/detail",
+    element: <DetailPage />,
+  },
+  {
+    path: "/my-bookings",
+    element: <MyBookingPage />,
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      <Home />
-      {/* <DetailPage />
-      <MyBookingPage /> */}
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
